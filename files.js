cef37a110629209eeb4b9c20ac2892b1ed2effcb
Britt <85905027+BFarmer26@users.noreply.github.com>
2024-06-24 19:55:43 -0400

    added more to the delete file section to add to the console file deleted once it has been deleted

--- files.js
+++ files.js
@@ -37,4 +37,11 @@ const fs = require('fs');
 //}
 
 // Deleting files
-if (fs.existsSync('./docs/deleteme.txt'))
+if (fs.existsSync('./docs/deleteme.txt')) {
+    fs.unlink('./docs/deleteme.txt', (err) => {
+    if (err) {
+        console.log(err)
+    }
+    console.log('file deleted');
+    })
+}
