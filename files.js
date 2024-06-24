56591bb9e387e206eafe587589cd3fc6da1925c4
Britt <85905027+BFarmer26@users.noreply.github.com>
2024-06-24 19:42:44 -0400

    changed last sections to all be comments

--- files.js
+++ files.js
@@ -1,23 +1,23 @@
 const fs = require('fs');
 
 // Reading files
-fs.readFile('./docs/blog1.txt', (err, data) => {
-if (err) {
-    console.log(err);
-}
-    console.log(data.toString());
-})
+//fs.readFile('./docs/blog1.txt', (err, data) => {
+//if (err) {
+ //   console.log(err);
+//}
+  //  console.log(data.toString());
+//})
 
-console.log('last line');
+//console.log('last line');
 
 // Writing files
-fs.writeFile('./docs/blog1.txt', 'hello, world', () => {
-    console.log('file was written');
-})
+//fs.writeFile('./docs/blog1.txt', 'hello, world', () => {
+  //  console.log('file was written');
+//})
 
-fs.writeFile('./docs/blog2.txt', 'hello, again', () => {
-    console.log('file was written');
-})
+//fs.writeFile('./docs/blog2.txt', 'hello, again', () => {
+  //  console.log('file was written');
+//})
 
 // Directories
 
