77b199e4c91b3c6b7f0382201dea794ded647755
Britt <85905027+BFarmer26@users.noreply.github.com>
2024-06-24 19:41:05 -0400

    added another writeFile that creates new txt

--- files.js
+++ files.js
@@ -15,6 +15,10 @@ fs.writeFile('./docs/blog1.txt', 'hello, world', () => {
     console.log('file was written');
 })
 
+fs.writeFile('./docs/blog2.txt', 'hello, again', () => {
+    console.log('file was written');
+})
+
 // Directories
 
 // Deleting files
